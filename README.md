fe3caf5146e0faf6844d14263fcb86630aaf3341
Anton Bannykh <anton.bannykh@gmail.com>
2017-11-26 20:53:00 +0300

    Sample project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build status](https://travis-ci.org/anton-bannykh/playground.svg?branch=master)](https://travis-ci.org/anton-bannykh/playground)
+
 # playground
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+    @Test public void testFoo() {
+        Assert.assertEquals(10, (new Main()).foo());
+    }
+}
