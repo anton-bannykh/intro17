d398f61d24cb5e8f9f901820b917a10998824c02
Mervap <valeratep@mail.ru>
2017-11-27 22:34:07 +0300

    Resolved algorithm
    Resolve algorithm, tests, check codestile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
-  int foo() {
-    return 10;
+  double median(int[] arr) {
+    Arrays.sort(arr);
+    int len = arr.length;
+    if (len == 0) {
+      return 0;
+    }
+    if (len % 2 == 1) {
+      return arr[len / 2];
+    } else {
+      return ((double) (arr[len / 2 - 1] + arr[len / 2]) / 2);
+    }
   }
 }
