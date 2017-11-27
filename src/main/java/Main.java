dbe4224c8a8e2cb4e4a751f785aa4ede38b74e62
BrilZliaN <brilzlian@ya.ru>
2017-11-27 19:09:25 +0300

    Added method for getting the sum of powers of 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
   int foo() {
     return 10;
   }
+  
+  int sumPowersOf2(int[] numbers) {
+    int sum = 0;
+    for (int number : numbers) {
+      if (number >= 0 && number < 31) {
+        sum += 1 << number;
+      }
+    }
+    return sum;
+  }
+  
 }
