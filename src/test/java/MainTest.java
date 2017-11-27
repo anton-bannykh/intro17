1ba6c6cf564d3d1f208c4f7368152135f3b02bd1
Nikita Sychev <root@nsychev.ru>
2017-11-27 18:31:36 +0500

    added two new tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -13,4 +13,14 @@ public class MainTest {
     assertEquals(6, (new Main()).sum(new int[] {1, 2, 3}));
     assertEquals(0, (new Main()).sum(new int[] {0, 0, 0}));
   }
+  
+  @Test
+  public void testSumEmpty() {
+    assertEquals(0, (new Main()).sum(new int[] {}));
+  }
+  
+  @Test
+  public void testSumNegative() {
+    assertEquals(-666, (new Main()).sum(new int[] {-111, -222, -333}));
+  }
 }
