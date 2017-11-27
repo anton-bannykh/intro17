ed9b2ee00660d60bf503dbb1fe22ee7a167a57f8
Arkady <arss860@gmail.com>
2017-11-27 22:33:41 +0300

    max function and tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,4 +6,20 @@ public class MainTest {
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
+
+  @Test
+  public void testMax1() {
+    Assert.assertEquals(5, (new Main()).max(new int[] {1,2,3,4,5}));
+  }
+
+  @Test
+  public void testMax2() {
+    Assert.assertEquals(3, (new Main()).max(new int[] {1,-2,3,-4,-5}));
+  }
+
+  @Test
+  public void testMaxEmpty() {
+    Assert.assertEquals(0, (new Main()).max(new int[] {}));
+  }
+
 }
