667f1276f9fbd2a232e9fbb4b46714216b7d5a2e
Starcall <trubetskoy.b.s@gmail.com>
2017-11-27 21:41:33 +0300

    Реализовал предложенный алгоритм. Добавил тестов на его проверку. Сделал gradlew check, прошел codestyle violation.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-  int foo() {
-    return 10;
+  int getMinimum(int[] inputArray) {
+    int mininmum = Integer.MAX_VALUE;
+    for (int i = 0; i < inputArray.length; i++) {
+      mininmum = Math.min(mininmum, inputArray[i]);
+    }
+    return mininmum;
+  }
+  
+  public static int main(String[] args) {
+    return 0;
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,7 +3,26 @@ import org.junit.Test;
 
 public class MainTest {
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void randomTest() {
+    int[] randomArray = {17, 3, 5};
+    Assert.assertEquals(3, (new Main()).getMinimum(randomArray));
+  }
+
+  @Test
+  public void randomTestWithNegativeValues() {
+    int[] randomArray = {-17, 0, 34};
+    Assert.assertEquals(-17, (new Main()).getMinimum(randomArray));
+  }
+
+  @Test
+  public void testOnSpecifiedCondition() {
+    int[] specifiedArray = {};
+    Assert.assertEquals(Integer.MAX_VALUE, (new Main()).getMinimum(specifiedArray));
+  }
+
+  @Test
+  public void oneTokenTest() {
+    int[] oneTokenArray = {1337};
+    Assert.assertEquals(1337, (new Main()).getMinimum(oneTokenArray));
   }
 }
