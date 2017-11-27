5cca659a01ac992b8369e7b6259b47f4c7a29574
Nikita Sychev <root@nsychev.ru>
2017-11-27 20:33:55 +0500

    added main method to sum all command-line args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,32 @@ public class Main {
     return 10;
   }
   
-  int sum(int[] arr) {
+  static int sum(int[] arr) {
     int ans = 0;
     for (int i = 0; i < arr.length; i++) {
       ans += arr[i];
     }
     return ans;
+  }
+
+  static String getSumOrError(String[] args) {
+    int[] nums = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      try {
+        nums[i] = Integer.parseInt(args[i]);
+      } catch (NumberFormatException exc) {
+        return String.format("Cannot parse integer at position %d", i);
+      }
+    }
+    return Integer.toString(sum(nums));
+  }
+
+  /**
+   * Calculates sum of given array and prints it to stdout.
+   *
+   * @param args Numbers to sum
+  **/
+  public static void main(String[] args) {
+    System.out.println(getSumOrError(args));
   }
 }
