cdc366c7fc56e8da95df644b57f4e24d73e172db
Nikita Sychev <root@nsychev.ru>
2017-11-27 17:36:36 +0500

    sum method implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,12 @@ public class Main {
   int foo() {
     return 10;
   }
+  
+  int sum(int[] arr) {
+    int ans = 0;
+    for (int i = 0; i < arr.length; i++) {
+      ans += arr[i];
+    }
+    return ans;
+  }
 }
