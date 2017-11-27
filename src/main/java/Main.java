687f3eb794fefb9e312f0f4c18dcd06c28c0e2ae
Yaroslav Sviridov <yarikbest7489@gmail.com>
2017-11-27 20:49:42 +0300

    Added tests and function for sum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-  int foo() {
-    return 10;
+public class Main { 
+  int sumDec(int[] arr) { 
+    int ans = 0;
+    for (int i = 0; i < arr.length; i++) { 
+      if (i % 2 == 0) { 
+        ans += arr[i];
+      } else { 
+        ans -= arr[i];
+      }
+    }
+    return ans;
   }
 }
