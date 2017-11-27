c22544d881923ec1c0a680a391f00a0e358c7dfb
BrilZliaN <brilzlian@ya.ru>
2017-11-27 22:18:42 +0300

    Task 3 - add support for command arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
 public class Main {
+  
+  /**
+   * Program entry point.
+   * @param args Array of strings containing numbers
+   */
+  public static void main(String[] args) {
+    Main that = new Main();
+    
+    System.out.println(that.sumPowersOf2(args));
+  }
 
   int foo() {
     return 10;
@@ -12,6 +22,28 @@ public class Main {
       }
     }
     return sum;
+  }
+  
+  /**
+   * A method that returns the sum of numbers of power of 2, 
+   * given in the array of strings.
+   * @param args An array of strings containing a number
+   * @return     The sum
+   */
+  public String sumPowersOf2(String[] args) {
+    //converting strings to integer
+    int[] numbers = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      String current = args[i];
+      try {
+        numbers[i] = Integer.parseInt(current);
+      } catch (NumberFormatException nfe) {
+        return "Wrong input format: \"" + args[i] + "\" is not an integer!";
+      }
+    }
+    //getting the answer
+    int answer = this.sumPowersOf2(numbers);
+    return String.valueOf(answer);
   }
   
 }
