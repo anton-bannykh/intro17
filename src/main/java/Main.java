7ba8aac79fc66f1608943a78167cd752181b875b
Arkady <arss860@gmail.com>
2017-11-27 22:33:41 +0300

    max function and tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,17 @@ public class Main {
   int foo() {
     return 10;
   }
+
+  int max(int[] arr) {
+    if (arr.length == 0) {
+      return 0;
+    }
+    int ans = arr[0];
+    for (int i = 0;i < arr.length; i++) {
+      if (ans < arr[i]) {
+        ans = arr[i];
+      }
+    }
+    return ans;
+  }
 }
