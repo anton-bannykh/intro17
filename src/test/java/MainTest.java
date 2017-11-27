a53b2e9a369e7d71aff2fe82ae4c89c500b7520e
Nikita Sychev <root@nsychev.ru>
2017-11-27 18:28:10 +0500

    changed import to static

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,15 +1,16 @@
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class MainTest {
   @Test
   public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+    assertEquals(10, (new Main()).foo());
   }
 
   @Test
   public void testSum() {
-    Assert.assertEquals(6, (new Main()).sum(new int[] {1, 2, 3}));
-    Assert.assertEquals(0, (new Main()).sum(new int[] {0, 0, 0}));
+    assertEquals(6, (new Main()).sum(new int[] {1, 2, 3}));
+    assertEquals(0, (new Main()).sum(new int[] {0, 0, 0}));
   }
 }
