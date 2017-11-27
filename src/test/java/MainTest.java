754be18c01ae342ca1ad63dc8eaebe9514005259
Nikita Sychev <root@nsychev.ru>
2017-11-27 17:37:08 +0500

    test for sum implemented

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,4 +6,10 @@ public class MainTest {
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
+
+  @Test
+  public void testSum() {
+    Assert.assertEquals(6, (new Main()).sum(new int[] {1, 2, 3}));
+    Assert.assertEquals(0, (new Main()).sum(new int[] {0, 0, 0}));
+  }
 }
