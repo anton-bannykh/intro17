ecdd71dd2c2fc820dc2aa0850cdad7a9b0d6ff43
Nikita Sychev <root@nsychev.ru>
2017-11-28 17:17:35 +0500

    fixes in codestyle

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,25 +10,31 @@ public class MainTest {
 
   @Test
   public void testSum() {
-    assertEquals(6, (new Main()).sum(new int[] {1, 2, 3}));
-    assertEquals(0, (new Main()).sum(new int[] {0, 0, 0}));
+    assertEquals(6, Main.sum(new int[] {1, 2, 3}));
+    assertEquals(0, Main.sum(new int[] {0, 0, 0}));
   }
   
   @Test
   public void testSumEmpty() {
-    assertEquals(0, (new Main()).sum(new int[] {}));
+    assertEquals(0, Main.sum(new int[] {}));
   }
   
   @Test
   public void testSumNegative() {
-    assertEquals(-666, (new Main()).sum(new int[] {-111, -222, -333}));
+    assertEquals(-666, Main.sum(new int[] {-111, -222, -333}));
   }
 
-  public static final String ERROR_TEXT = "Cannot parse integer at position %d";
+  static final String ERROR_TEXT_1 = "Cannot parse integer at position 0";
+  static final String ERROR_TEXT_2 = "Cannot parse integer at position 4";
+
+  @Test
+  public void testSumString() {
+    assertEquals("0", Main.getSumOrError(new String[] {"1", "2", "-3"}));
+  }
 
   @Test
   public void testSumError() {
-    assertEquals(String.format(ERROR_TEXT, 0), (new Main()).getSumOrError(new String[] {"a"}));
-    assertEquals("0", (new Main()).getSumOrError(new String[] {"1", "2", "-3"}));
+    assertEquals(ERROR_TEXT_1, Main.getSumOrError(new String[] {"a"}));
+    assertEquals(ERROR_TEXT_2, Main.getSumOrError(new String[] {"0", "17", "34", "51", "hello"}));
   }
 }
