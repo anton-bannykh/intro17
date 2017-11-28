b86a58ed4f387431decd45ed7a0449e4349bf1cf
Arkady <arss860@gmail.com>
2017-11-28 19:55:31 +0300

    some changes

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,25 +1,31 @@
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class MainTest {
   @Test
   public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+    assertEquals(10, (new Main()).foo());
   }
 
   @Test
-  public void testMax1() {
-    Assert.assertEquals(5, (new Main()).max(new int[] {1,2,3,4,5}));
+  public void testMax_randomarray1() {
+    assertEquals(5, (new Main()).max(new int[] {1,2,3,4,5}));
   }
 
   @Test
-  public void testMax2() {
-    Assert.assertEquals(3, (new Main()).max(new int[] {1,-2,3,-4,-5}));
+  public void testMax_randomarray2() {
+    assertEquals(3, (new Main()).max(new int[] {1,-2,3,-4,-5}));
   }
 
   @Test
-  public void testMaxEmpty() {
-    Assert.assertEquals(0, (new Main()).max(new int[] {}));
+  public void testMax_Empty() {
+    assertEquals(0, (new Main()).max(new int[] {}));
+  }
+
+  @Test
+  public void testMax_Negative() {
+    assertEquals(-5, (new Main()).max(new int[] {-5, -35, -100, -12}));
   }
 
 }
