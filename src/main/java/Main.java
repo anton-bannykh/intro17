2dc436e4034252e016b7776cb53721257ec45d5d
Mervap <valeratep@mail.ru>
2017-11-29 22:06:17 +0300

    Task 3
    Modify Main to console input/output, error handler for invalid input, fix output *integer_part*.0 instead *integer part* (3.0 instead 3)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
-  int foo() {
-    return 10;
+
+  public static void main(String[] args) {
+    System.out.println(median(args));
+  }
+
+  static String median(String[] args) {
+    int[] arr = new int[args.length];
+    for (int i = 0; i < args.length; ++i) {
+      try {
+        arr[i] = Integer.parseInt(args[i]);
+      } catch (NumberFormatException exc) {
+        return ("You wrote not a number!");
+      }
+    }
+
+    Arrays.sort(arr);
+    int len = arr.length;
+    if (len == 0) {
+      return String.valueOf(0);
+    }
+    if (len % 2 == 1) {
+      return String.valueOf(arr[len / 2]);
+    } else {
+      if ((arr[len / 2 - 1] + arr[len / 2]) % 2 == 0) {
+        return String.valueOf((arr[len / 2 - 1] + arr[len / 2]) / 2);
+      } else {
+        return String.valueOf((double) (arr[len / 2 - 1] + arr[len / 2]) / 2);
+      }
+    }
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,8 +2,46 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
+
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testOddPos() {
+    String[] arr = {"5", "2", "1", "4", "3"};
+    Assert.assertEquals("3", (new Main()).median(arr));
+  }
+
+  @Test
+  public void testEvenPos1() {
+    String[] arr = {"5", "2", "1", "4", "3", "6"};
+    Assert.assertEquals("3.5", (new Main()).median(arr));
+  }
+
+  @Test
+  public void testEvenPos2() {
+    String[] arr = {"5", "2", "1", "7", "3", "6"};
+    Assert.assertEquals("4", (new Main()).median(arr));
+  }
+
+  @Test
+  public void testOddNeg() {
+    String[] arr = {"-5", "-2", "-1", "-4", "-3"};
+    Assert.assertEquals("-3", (new Main()).median(arr));
+  }
+
+  @Test
+  public void testEvenNeg() {
+    String[] arr = {"-5", "-2", "-1", "-4", "-3", "-6"};
+    Assert.assertEquals("-3.5", (new Main()).median(arr));
+  }
+
+  @Test
+  public void testNull() {
+    String[] arr = {};
+    Assert.assertEquals("0", (new Main()).median(arr));
+  }
+
+  @Test
+  public void testWrong() {
+    String[] arr = {"5", "4", "kek", "-5"};
+    Assert.assertEquals("You wrote not a number!", (new Main()).median(arr));
   }
 }
