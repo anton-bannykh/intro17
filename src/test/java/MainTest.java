9c8d892373aee5558b51af733dc420c2f81fdaf5
Mervap <valeratep@mail.ru>
2017-11-29 20:46:46 +0300

    Task 2
    Resolve algorithm, check codestyle

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,8 +2,35 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
+  double eps = 1e-9;
+  
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testOddPos() {
+    int[] arr = {5, 2, 1, 4, 3};
+    Assert.assertEquals(3, (new Main()).median(arr), eps);
+  }
+
+  @Test
+  public void testEvenPos() {
+    int[] arr = {5, 2, 1, 4, 3, 6};
+    Assert.assertEquals(3.5, (new Main()).median(arr), eps);
+  }
+
+  @Test
+  public void testOddNeg() {
+    int[] arr = {-5, -2, -1, -4, -3};
+    Assert.assertEquals(-3, (new Main()).median(arr), eps);
+  }
+
+  @Test
+  public void testEvenNeg() {
+    int[] arr = {-5, -2, -1, -4, -3, -6};
+    Assert.assertEquals(-3.5, (new Main()).median(arr), eps);
+  }
+
+  @Test
+  public void testNull() {
+    int[] arr = {};
+    Assert.assertEquals(0, (new Main()).median(arr), eps);
   }
 }
