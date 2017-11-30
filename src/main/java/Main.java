b5b4cae3cde59049c28ede57b2f06f5560999ca8
Prilog <sementrapeznikov@gmail.com>
2017-11-30 23:49:43 +0300

    Task 2
    Solved task in Main.java
    Added tests in MainTest.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
 public class Main {
-  int foo() {
-    return 10;
+
+  private int gcd(int arg1, int arg2) {
+    if (arg2 == 0) {
+      return arg1;
+    } else {
+      return gcd(arg2, arg1 % arg2);
+    }
+  }
+
+  /** This shit counts arrays gcd. */
+  public int nekogcd(int[] arg) {
+    int res = 0;
+    for (int i = 0; i < arg.length; i++) {
+      res = gcd(res, arg[i]);
+    }
+    if (res == 0) {
+      return Integer.MAX_VALUE;
+    }
+    return res;
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,8 +2,40 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
+
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testGcd1() {
+    int[] arr = {};
+    Assert.assertEquals(Integer.MAX_VALUE, (new Main()).nekogcd(arr));
+  }
+
+  @Test
+  public void testGcd2() {
+    int[] arr = {0, 0, 0, 0, 0};
+    Assert.assertEquals(Integer.MAX_VALUE, (new Main()).nekogcd(arr));
+  }
+
+  @Test
+  public void testGcd3() {
+    int[] arr = {228};
+    Assert.assertEquals(228, (new Main()).nekogcd(arr));
+  }
+
+  @Test
+  public void testGcd4() {
+    int[] arr = {2, 4, 18, 98};
+    Assert.assertEquals(2, (new Main()).nekogcd(arr));
+  }
+
+  @Test
+  public void testGcd5() {
+    int[] arr = {3072, 72, 15984};
+    Assert.assertEquals(24, (new Main()).nekogcd(arr));
+  }
+
+  @Test
+  public void testGcd6() {
+    int[] arr = {215784, 298404, 73872, 39852, 104328, 118584, 6480};
+    Assert.assertEquals(324, (new Main()).nekogcd(arr));
   }
 }
