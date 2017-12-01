d26d99e833879019611aecd5ca6ea3d4714fcc34
Mervap <valeratep@mail.ru>
2017-12-01 20:06:43 +0300

    Task 3
    Modify Main to console input/output, error handler for invalid input, fix output *integer_part*.0 instead *integer part* (3.0 instead 3)

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,35 +2,46 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
-  double eps = 1e-9;
-  
+
   @Test
   public void testOddPos() {
-    int[] arr = {5, 2, 1, 4, 3};
-    Assert.assertEquals(3, (new Main()).median(arr), eps);
+    String[] arr = {"5", "2", "1", "4", "3"};
+    Assert.assertEquals("3", (new Main()).median(arr));
   }
 
   @Test
-  public void testEvenPos() {
-    int[] arr = {5, 2, 1, 4, 3, 6};
-    Assert.assertEquals(3.5, (new Main()).median(arr), eps);
+  public void testEvenPos1() {
+    String[] arr = {"5", "2", "1", "4", "3", "6"};
+    Assert.assertEquals("3.5", (new Main()).median(arr));
+  }
+
+  @Test
+  public void testEvenPos2() {
+    String[] arr = {"5", "2", "1", "7", "3", "6"};
+    Assert.assertEquals("4", (new Main()).median(arr));
   }
 
   @Test
   public void testOddNeg() {
-    int[] arr = {-5, -2, -1, -4, -3};
-    Assert.assertEquals(-3, (new Main()).median(arr), eps);
+    String[] arr = {"-5", "-2", "-1", "-4", "-3"};
+    Assert.assertEquals("-3", (new Main()).median(arr));
   }
 
   @Test
   public void testEvenNeg() {
-    int[] arr = {-5, -2, -1, -4, -3, -6};
-    Assert.assertEquals(-3.5, (new Main()).median(arr), eps);
+    String[] arr = {"-5", "-2", "-1", "-4", "-3", "-6"};
+    Assert.assertEquals("-3.5", (new Main()).median(arr));
   }
 
   @Test
   public void testNull() {
-    int[] arr = {};
-    Assert.assertEquals(0, (new Main()).median(arr), eps);
+    String[] arr = {};
+    Assert.assertEquals("0", (new Main()).median(arr));
+  }
+
+  @Test
+  public void testWrong() {
+    String[] arr = {"5", "4", "kek", "-5"};
+    Assert.assertEquals("You wrote not a number!", (new Main()).median(arr));
   }
 }
