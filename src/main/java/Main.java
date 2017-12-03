b801bad93c0362b3c43d0a39552fe18e9a047765
Dmitry <LaletinDN@mail.ru>
2017-12-03 23:04:49 +0300

    Add Task 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-  int foo() {
-    return 10;
+  public static void main(String[] args) {
+    //System.out.println(sum_of_power5());
   }
+
+  public static int sum_of_power5(int[] power){
+    final int FIVE = 5;
+    int res = 0;
+    for(int digit : power){
+      if(digit >= 0 && digit <= 10){
+        res += pow(FIVE, digit);
+      }
+    }
+    return res;
+  }
+
+  private static int pow(int a, int b){
+    int res = 1;
+    for(int i = 1; i <= b; ++i){
+      res = res * a;
+    }
+    return res;
+  }
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,49 @@
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testSimple(){
+    assertEquals(30, Main.sum_of_power5(new int[]{1,2}));
   }
+
+  @Test
+  public void testIgnoreLargeNumbers(){
+    assertEquals(30, Main.sum_of_power5(new int[]{11, 1, 13, 2}));
+  }
+
+  @Test
+  public void testAllNumbersIs0(){
+    assertEquals(3, Main.sum_of_power5(new int[]{0, 0, 0}));
+  }
+
+  @Test
+  public void testSingleNumber(){
+    assertEquals(25, Main.sum_of_power5(new int[]{2}));
+  }
+
+  @Test
+  public void testEmptyInput(){
+    assertEquals(0, Main.sum_of_power5(new int[]{}));
+  }
+
+  @Test
+  public void allCasesTogether(){
+    assertEquals(3256, Main.sum_of_power5(new int[]{3, 0, 5, 14, 1, 15}));
+  }
+
+  @Test
+  public void speakingName(){
+    assertEquals(78380, Main.sum_of_power5(new int[]{1, 3, 3, 7}));
+  }
+
+  @Ignore
+  @Test(timeout = 1000)
+  public void speedTest(){
+    assertEquals(12207030,Main.sum_of_power5(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+  }
+
 }
