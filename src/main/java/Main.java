8af7073d98ee6564238c7865cb53779609a3a2a3
Arslan <arimionim@yandex.ru>
2017-12-04 01:05:04 +0500

    add class which do needed algth

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,15 @@ public class Main {
   int foo() {
     return 10;
   }
-  public int sumOfMul(int[] arr){
-		int ans = 0;
-		for (int i = 0; i < arr.length; i += 2){
-			ans = arr[i] * arr[i   1];
-		}
-		if (i % 2){
-			ans = arr[arr.length - 1];
-		}
-		return ans;
-	}
+
+  public int sumOfMul(int[] arr) {
+    int ans = 0;
+    for (int i = 0; i < arr.length; i += 2) {
+      ans = arr[i] * arr[i + 1];
+    }
+    if (arr.length % 2 == 1) {
+      ans = arr[arr.length - 1];
+    }
+    return ans;
+  }
 }
