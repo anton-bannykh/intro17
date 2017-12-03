544e63e329030645b856c81a1825f35565e06366
Dmitry <LaletinDN@mail.ru>
2017-12-04 01:19:56 +0300

    Fixed CodeStyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,24 @@
 public class Main {
-  public static void main(String[] args) {
-    //System.out.println(sum_of_power5());
-  }
 
-  public static int sum_of_power5(int[] power){
-    final int FIVE = 5;
+  /**
+   * @param power array of power.
+   * @return sum of powers 5.
+   */
+  public static int sumOfPower5(int[] power) {
     int res = 0;
-    for(int digit : power){
-      if(digit >= 0 && digit <= 10){
-        res += pow(FIVE, digit);
+    for (int digit : power) {
+      if (digit >= 0 && digit <= 10) {
+        res += pow(5, digit);
       }
     }
     return res;
   }
 
-  private static int pow(int a, int b){
+  // Exponentiation function
+  private static int pow(int digit, int exp) {
     int res = 1;
-    for(int i = 1; i <= b; ++i){
-      res = res * a;
+    for (int i = 1; i <= exp; ++i) {
+      res = res * digit;
     }
     return res;
   }
