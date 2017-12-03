31b350e092f5200579be706d9d66b89e6059eb5c
Arslan <arimionim@yandex.ru>
2017-12-04 00:24:42 +0500

    added class which do requested algorithm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,14 @@ public class Main {
   int foo() {
     return 10;
   }
+  public int sumOfMul(int[] arr){
+		int ans = 0;
+		for (int i = 0; i < arr.length; i += 2){
+			ans = arr[i] * arr[i   1];
+		}
+		if (i % 2){
+			ans = arr[arr.length - 1];
+		}
+		return ans;
+	}
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,43 @@
-import org.junit.Assert;
-import org.junit.Test;
-
 public class MainTest {
-  @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
-  }
-}
+    @Test
+    public void testEmpty() {
+      int[] arr = {};
+      assertEquals(0, (new Main()).sum_of_mul(arr));
+    }
+  
+    @Test
+    public void testSingleNumber() {
+      int[] arr = {228};
+      assertEquals(228, (new Main()).sum_of_mul(arr));
+    }
+  
+    @Test
+    public void testSomeMumbers() {
+      int[] arr = {2, 4, 18, 98};
+      assertEquals(2, (new Main()).sum_of_mul(arr));
+    }
+  
+    @Test
+    public void testOnes() {
+      int[] arr = {1, 1, 1, 1, 1};
+      assertEquals(3, (new Main()).sum_of_mul(arr));
+    }
+  
+    @Test
+    public void testSomeEqualNumbers() {
+      int[] arr = {123, 123, 123};
+      assertEquals(15252, (new Main()).sum_of_mul(arr));
+    }
+  
+    @Test
+    public void testZeros() {
+      int[] arr = {0, 0, 0, 0};
+      assertEquals(0, (new Main()).sum_of_mul(arr));
+    }
+  
+    @Test
+    public void testPermutation() {
+      int[] arr = {1, 2, 3, 4, 5, 7};
+      assertEquals(68, (new Main()).sum_of_mul(arr));
+    }
+ -}
