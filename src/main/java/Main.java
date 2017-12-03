abe4c85ece1ea1cbd1987710e164f8ef0662b6ca
Donat Sokolov <donatsokolov99@gmail.com>
2017-12-03 22:57:46 +0300

    Implement Second and Third tasks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-  int foo() {
-    return 10;
+  /**
+   * The entery point of the program.
+   *
+   * @param  args  arguments
+   */
+  public static void main(String[] args) {
+    try {
+      int sum = Utils.fibonacciSum(Utils.createArray(args));
+      System.out.println(sum);
+    } catch (CreateArrayException exception) {
+      System.out.println(exception.getMessage());
+    }
   }
 }
---
+++ src/test/java/UtilsTest.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UtilsTest {
+  @Test
+  public void testUtils() {
+    int[] array = new int[5];
+    String[] str = new String[5];
+    for (int ind = 0; ind < str.length; ++ind) {
+      str[ind] = ((Integer) ind).toString();
+      array[ind] = ind;
+    }
+    int[] utilsArray = Utils.createArray(str);
+    Assert.assertArrayEquals(utilsArray, array);
+  }
+
+  @Test(expected = CreateArrayException.class)
+  public void testUtilsWithError() {
+    String[] str = new String[5];
+    for (int ind = 0; ind < str.length; ++ind) {
+      str[ind] = ((Integer) ind).toString();
+    }
+    str[3] = "A";
+    int[] utilsArray = Utils.createArray(str);
+  }
+
+  @Test
+  public void testFibonacci() {
+    Assert.assertEquals(Utils.fibonacciSum(new int[]{0, 1, 3}), 5);
+  }
+
+  @Test
+  public void testFibonacciWithZero() {
+    Assert.assertEquals(Utils.fibonacciSum(new int[]{-1, 0, 1, 3}), 5);
+    Assert.assertEquals(Utils.fibonacciSum(new int[]{16, 0, 1, 3}), 5);
+  }
+}
