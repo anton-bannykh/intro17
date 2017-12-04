ed8aa36967fbbe93b228dfb00dd2e809b729f3c8
Dmitry <LaletinDN@mail.ru>
2017-12-04 10:52:45 +0300

    Add Task 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
 public class Main {
-  int foo() {
-    return 10;
+
+  /**
+   * @param power array of power.
+   * @return sum of powers 5.
+   */
+  public static int sumOfPower5(int[] power) {
+    int res = 0;
+    for (int digit : power) {
+      if (digit >= 0 && digit <= 10) {
+        res += pow(5, digit);
+      }
+    }
+    return res;
   }
+
+  // Exponentiation function
+  private static int pow(int digit, int exp) {
+    int res = 1;
+    for (int i = 1; i <= exp; ++i) {
+      res = res * digit;
+    }
+    return res;
+  }
+
 }
