41252d270427780360f319bdd6e560138584dc7e
Arslan <arimionim@yandex.ru>
2017-12-05 01:49:16 +0500

    added class which does needed algthm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
   int sumOfMul(int[] arr) {
     int ans = 0;
-    for (int i = 0; i < arr.length; i += 2) {
-      ans = arr[i] * arr[i + 1];
+    for (int i = 1; i < arr.length; i += 2) {
+      ans = arr[i] * arr[i - 1];
     }
     if (arr.length % 2 == 1) {
       ans = arr[arr.length - 1];
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,42 +2,42 @@ public class MainTest {
  
     public void testEmpty() {
       int[] arr = {};
-      assertEquals(0, (new Main()).sum_of_mul(arr));
+      assertEquals(0, (new Main()).sumOfMul(arr));
     }
   
  
     public void testSingleNumber() {
       int[] arr = {228};
-      assertEquals(228, (new Main()).sum_of_mul(arr));
+      assertEquals(228, (new Main()).sumOfMul(arr));
     }
   
  
     public void testSomeMumbers() {
       int[] arr = {2, 4, 18, 98};
-      assertEquals(2, (new Main()).sum_of_mul(arr));
+      assertEquals(1772, (new Main()).sumOfMul(arr));
     }
   
  
     public void testOnes() {
       int[] arr = {1, 1, 1, 1, 1};
-      assertEquals(3, (new Main()).sum_of_mul(arr));
+      assertEquals(3, (new Main()).sumOfMul(arr));
     }
   
  
     public void testSomeEqualNumbers() {
       int[] arr = {123, 123, 123};
-      assertEquals(15252, (new Main()).sum_of_mul(arr));
+      assertEquals(15252, (new Main()).sumOfMul(arr));
     }
   
  
     public void testZeros() {
       int[] arr = {0, 0, 0, 0};
-      assertEquals(0, (new Main()).sum_of_mul(arr));
+      assertEquals(0, (new Main()).sumOfMul(arr));
     }
   
  
     public void testPermutation() {
       int[] arr = {1, 2, 3, 4, 5, 7};
-      assertEquals(68, (new Main()).sum_of_mul(arr));
+      assertEquals(68, (new Main()).sumOfMul(arr));
     }
  }
