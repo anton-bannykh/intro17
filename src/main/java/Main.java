0c8fa9722dd58f58fe289bac420e5323a2872da8
gerind <gerind@yandex.ru>
2017-12-04 16:52:34 +0300

    Created algorithm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
-  int foo() {
-    return 10;
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] mas = new int[sc.nextInt()];
+    for (int i = 0; i < mas.length; i++)
+      mas[i] = sc.nextInt();
+    if (foo(mas))
+      System.out.println("Yes");
+    else
+      System.out.println("No");
+  }
+  public static boolean foo(int[] mas) {
+    int s = 0;
+    for (int i = 0; i < mas.length; i++)
+      s ^= mas[i];
+    return ((s & 1) == 0);
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 public class MainTest {
   @Test
   public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+    Assert.assertEquals(true, (new Main()).foo(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}));
+    Assert.assertEquals(false, (new Main()).foo(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    Assert.assertEquals(false, (new Main()).foo(new int[]{1}));
+    Assert.assertEquals(true, (new Main()).foo(new int[]{2}));
   }
 }
