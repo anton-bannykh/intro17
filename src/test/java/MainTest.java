dd13b5a778a1f0281b7a3fee0241fd2dc26fbafd
Arslan <arimionim@yandex.ru>
2017-12-05 03:00:16 +0500

    some fixes

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,46 +1,48 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 public class MainTest {
   
-  @test
+  @Test
   public void testEmpty() {
     int[] arr = {};
     assertEquals(0, (new Main()).sumOfMul(arr));
   }
   
-  @test
+  @Test
   public void testSingleNumber() {
     int[] arr = {228};
     assertEquals(228, (new Main()).sumOfMul(arr));
   }
   
-  @test
+  @Test
   public void testSomeMumbers() {
     int[] arr = {2, 4, 18, 98};
     assertEquals(1772, (new Main()).sumOfMul(arr));
   }
   
-  @test
+  @Test
   public void testOnes() {
     int[] arr = {1, 1, 1, 1, 1};
     assertEquals(3, (new Main()).sumOfMul(arr));
   }
  
-  @test
+  @Test
   public void testSomeEqualNumbers() {
     int[] arr = {123, 123, 123};
     assertEquals(15252, (new Main()).sumOfMul(arr));
   }
   
-  @test
+  @Test
   public void testZeros() {
     int[] arr = {0, 0, 0, 0};
     assertEquals(0, (new Main()).sumOfMul(arr));
   }
   
-  @test
+  @Test
   public void testPermutation() {
     int[] arr = {1, 2, 3, 4, 5, 7};
-    assertEquals(68, (new Main()).sumOfMul(arr));
+    assertEquals(49, (new Main()).sumOfMul(arr));
   }
 }
