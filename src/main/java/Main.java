6e62f27d8aec74eef2d5e178d111d8701d863c04
Arslan <arimionim@yandex.ru>
2017-12-05 02:41:19 +0500

    added class which does needed algthm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,15 @@ public class Main {
   int foo() {
     return 10;
   }
+
+  int sumOfMul(int[] arr) {
+    int ans = 0;
+    for (int i = 1; i < arr.length; i += 2) {
+      ans = arr[i] * arr[i - 1];
+    }
+    if (arr.length % 2 == 1) {
+      ans = arr[arr.length - 1];
+    }
+    return ans;
+  }
 }
