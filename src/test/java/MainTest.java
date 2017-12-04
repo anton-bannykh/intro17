e23fc3c80c181eb173ed7b93a49ceed11b74bac3
Artyom Ustinov <artyom.ustinov99@yandex.ru>
2017-12-04 17:47:19 +0300

    Added test for sum

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,4 +6,19 @@ public class MainTest {
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
+
+  @Test
+  public void CorrectSum() {
+    Assert.assertEquals(13, (new Main()).SumOfPowers(new int[] {0,1,2}));
+  }
+
+  @Test
+  public void TestSmallArgs() {
+    Assert.assertEquals(1, (new Main()).SumOfPowers(new int[] {-1,0,-2,-345}));
+  }
+
+  @Test
+  public void TestBigArgs() {
+    Assert.assertEquals(3, (new Main()).SumOfPowers(new int[] {1,16,17,999}));
+  }
 }
