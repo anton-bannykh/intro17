4d8eda20849a57c905d67ec15974868d637bf2ff
Prilog <sementrapeznikov@gmail.com>
2017-12-04 21:47:10 +0300

    Task 3
    Modernised into "console application". Praise the Ship Girls!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
 public class Main {
 
+  public static void main(String[] args) {
+    System.out.println(nekoConsoleGcd(args));
+  }
+
   /**
-   * Simple realisation of euclids GCD.
+   * Makes int array from string array. Looks for errors. Makes output string.
    */
-  private int gcd(int arg1, int arg2) {
+  public static String nekoConsoleGcd(String[] args) {
+    int[] inputArray = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      try {
+        inputArray[i] = Integer.parseInt(args[i]);
+      } catch (NumberFormatException error) {
+        return String.format("Infernal error: wrong input: %s. Ochen zhal.", error.getMessage());
+      }
+    }
+    return Integer.toString(nekogcd(inputArray));
+  }
+
+  /**
+   * Simple implementation of euclids GCD.
+   */
+  private static int gcd(int arg1, int arg2) {
     if (arg2 == 0) {
       return arg1;
     } else {
@@ -14,7 +33,7 @@ public class Main {
   /**
    * This method counts arrays gcd.
    */
-  public int nekogcd(int[] arg) {
+  public static int nekogcd(int[] arg) {
     int res = 0;
     for (int i = 0; i < arg.length; i++) {
       res = gcd(res, Math.abs(arg[i]));
