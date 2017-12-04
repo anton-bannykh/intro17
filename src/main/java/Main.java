17540cc6044e21880a7eda01452097e36483aee1
Artyom Ustinov <artyom.ustinov99@yandex.ru>
2017-12-04 17:48:07 +0300

    Added new method for calculating sum of pows of 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,13 @@ public class Main {
   int foo() {
     return 10;
   }
+  int SumOfPowers(int[] args){
+    int sum=0;
+    for (int a:args){
+      if (a>=0&&a<=15) {
+        sum += Math.pow(3,a);
+      }
+    }
+    return sum;
+  }
 }
