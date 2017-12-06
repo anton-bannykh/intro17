04ec7e8d9d31cb94e8b7a150d7e5515e83037aa4
Prilog <sementrapeznikov@gmail.com>
2017-12-06 14:55:49 +0300

    Revert "Task 3"
    This reverts commit 93d4e1acb1b80462216c73bd4c89bd05668e7915.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,84 +1,9 @@
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
-
   @Test
-  public void testGcd1_empty() {
-    int[] arr = {};
-    assertEquals(Integer.MAX_VALUE, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testGcd2_allzero() {
-    int[] arr = {0, 0, 0, 0, 0};
-    assertEquals(Integer.MAX_VALUE, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testGcd3_singlenumber() {
-    int[] arr = {228};
-    assertEquals(228, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testGcd4_onesimple() {
-    int[] arr = {2, 4, 18, 98};
-    assertEquals(2, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testGcd5_complex() {
-    int[] arr = {3072, 72, 15984};
-    assertEquals(24, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testGcd6_complexbig() {
-    int[] arr = {215784, 298404, 73872, 39852, 104328, 118584, 6480};
-    assertEquals(324, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testGcd7_singlenegative() {
-    int[] arr = {-38};
-    assertEquals(38, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testGcd8_somenegatives() {
-    int[] arr = {-99, -27, -18};
-    assertEquals(9, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testGcd9_negativeandpositive() {
-    int[] arr = {215784, -298404, 73872, -39852, 104328, 118584, -6480};
-    assertEquals(324, (new Main()).nekogcd(arr));
-  }
-
-  @Test
-  public void testConsoleInput1_allok() {
-    String[] testArray = "215784 -298404 73872 -39852 104328 118584 -6480".split(" ");
-    assertEquals("324", (new Main()).nekoConsoleGcd(testArray));
-  }
-
-  @Test
-  public void testConsoleInput2_empty() {
-    assertEquals("2147483647", (new Main()).nekoConsoleGcd(new String[0]));
-  }
-
-  @Test
-  public void testConsoleInput3_zeros() {
-    assertEquals("2147483647", (new Main()).nekoConsoleGcd("0 00 0000 0 00".split(" ")));
-  }
-
-  @Test
-  public void testConsoleInput4_wronginput() {
-    String[] testArray = "abc 123f fff3 .lk".split(" ");
-    String expectedAnswer = "Infernal error: wrong input: For input string: \"abc\". Ochen zhal.";
-    assertEquals(expectedAnswer, (new Main()).nekoConsoleGcd(testArray));
+  public void testFoo() {
+    Assert.assertEquals(10, (new Main()).foo());
   }
 }
