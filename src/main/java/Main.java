23f589e132aac5f20b06d355b31fd338da6f25e1
anastasiaKretova <kretova_anastasia@mail.ru>
2017-12-10 04:49:03 +0300

    Task 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,13 @@ public class Main {
   int foo() {
     return 10;
   }
+
+  static int sumTrailingZeros(int[] arr) {
+    int ans = 0;
+    for (int i = 0; i < arr.length; i++) {
+      int num = Integer.numberOfTrailingZeros(arr[i]);
+      ans += (num == 32 ? 0 : num);
+    }
+    return ans;
+  }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,4 +6,24 @@ public class MainTest {
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
+
+  @Test
+  public void testSumTrailingZeroes() {
+    Assert.assertEquals(4, Main.sumTrailingZeros(new int[] {10, 8}));
+  }
+
+  @Test
+  public void testSumZero() {
+    Assert.assertEquals(0, Main.sumTrailingZeros(new int[] {7, 15, 127}));
+  }
+
+  @Test
+  public void testSumTrailingZeroes2() {
+    Assert.assertEquals(7, Main.sumTrailingZeros(new int[] {7, 8, 9, 10, 11, 12, 13, 14, 15}));
+  }
+
+  @Test
+  public void testSumZero2() {
+    Assert.assertEquals(0, Main.sumTrailingZeros(new int[] {3, 31, 63, 255}));
+  }
 }
