61b2ec18e3c6a3b19409129bb6eec1f2ac9108d3
Dmitry <LaletinDN@mail.ru>
2017-12-10 03:37:35 +0300

    Add Task 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+  public static void main(String[] args) {
+    System.out.println(sumOfPower5(args));
+  }
 
   /**
    * @param power array of power.
@@ -12,6 +15,22 @@ public class Main {
       }
     }
     return res;
+  }
+
+  /**
+   * The function is called if the input data is strings.
+   */
+  public static String sumOfPower5(String[] args) {
+    int[] power = new int[args.length];
+
+    for (int i = 0; i < power.length; i++) {
+      try {
+        power[i] = Integer.parseInt(args[i]);
+      } catch (NumberFormatException error) {
+        return "Неверный формат входных данных";
+      }
+    }
+    return Integer.toString(sumOfPower5(power));
   }
 
   // Exponentiation function
