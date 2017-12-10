f050cc60496585d37fcda5a35652151a56490daf
Yaroslav Sviridov <yarikbest7489@gmail.com>
2017-12-11 00:50:16 +0300

    Added tests and reading from cmd line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
 public class Main { 
-  int sumDec(int[] arr) { 
+
+  public static void main(String[] args) {
+    System.out.println(sumDec(args));
+  }
+
+  static String sumDec(String[] args) {
+    int[] arr = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      try {
+        arr[i] = Integer.parseInt(args[i]);
+      } catch (NumberFormatException ex) {
+        return ("Bad numbers!");
+      }
+    }
     int ans = 0;
     for (int i = 0; i < arr.length; i++) { 
       if (i % 2 == 0) { 
@@ -8,6 +21,6 @@ public class Main {
         ans -= arr[i];
       }
     }
-    return ans;
+    return String.valueOf(ans);
   }
 }
