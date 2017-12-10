47556f1c265d6a84a9d02a9989c9ab8beb2bc3d1
unknown <vova@s.w>
2017-12-11 00:11:10 +0300

    tried to do smth like task 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-  int foo() {
-    return 10;
+  int ssum(int[] arr) { // sum of numbers in index pow
+    int ans = 0;
+    for (int i = 1; i <= arr.length; i++) {
+      double pp = Math.pow((double)(arr[i - 1]),(double)i);
+      ans += (int)pp;
+    }
+    return ans;
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,41 @@
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class MainTest {
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testNull() {
+    int[] arr = {};
+    assertEquals(0, (new Main()).ssum(arr));
+  }
+
+  @Test
+  public void testPos() {
+    int[] arr = {6, 6};
+    assertEquals(42, (new Main()).ssum(arr));
+  }
+
+  @Test
+  public void testNeg() {
+    int[] arr = {-8, -8};
+    assertEquals(56, (new Main()).ssum(arr));
+  }
+
+  @Test
+  public void testZero() {
+    int[] arr = {0, 0};
+    assertEquals(0, (new Main()).ssum(arr));
+  }
+
+  @Test
+  public void testShort() {
+    int[] arr = {6, -3, 10};
+    assertEquals(1015, (new Main()).ssum(arr));
+  }
+
+  @Test
+  public void testLong() {
+    int[] arr = {-3, -2, -8, 3, 5, 0, 1, -1};
+    assertEquals(2697, (new Main()).ssum(arr));
   }
 }
