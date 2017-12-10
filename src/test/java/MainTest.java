b703f9d794dd714376c5711196b69a8c749c0882
Yaroslav Sviridov <yarikbest7489@gmail.com>
2017-12-11 00:50:16 +0300

    Added tests and reading from cmd line

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,21 +6,26 @@ import org.junit.Test;
 public class MainTest {
   @Test
   public void testSumDecEmpty() {
-    assertEquals(0, (new Main()).sumDec(new int[]{}));
+    assertEquals("0", (new Main()).sumDec(new String[]{}));
   }
   
   @Test
   public void testSumDecPositive() {
-    assertEquals(6, (new Main()).sumDec(new int[]{5, 6, 7}));
+    assertEquals("6", (new Main()).sumDec(new String[]{"5", "6", "7"}));
   }
   
   @Test
   public void testSumDecNegative() {
-    assertEquals(-3, (new Main()).sumDec(new int[]{-2, -6, -7}));
+    assertEquals("-3", (new Main()).sumDec(new String[]{"-2", "-6", "-7"}));
   }
   
   @Test
   public void testSumDecCombine() {
-    assertEquals(-22, (new Main()).sumDec(new int[]{3, 5, -8, 12}));
+    assertEquals("-22", (new Main()).sumDec(new String[]{"3", "5", "-8", "12"}));
+  }
+  
+  @Test
+  public void testSumDecWrongInputWords() {
+    assertEquals("Bad numbers!", (new Main()).sumDec(new String[]{"3", "kek", "ne kek", ""}));
   }
 }
