1be4726be2c94e01c1188d754968d9c022ecd8c8
botal9 <vitaliy.ovechkin.99@mail.ru>
2017-12-11 01:22:43 +0300

    Task3. Added console aplication. It calculates sum of highest bits of integer array and checks for errors.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,14 +2,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
+
+  static final String ERROR_NEGATIVE = "Elements of array mustn't be negative.";
+  static final String ERROR_STRING = "Can't parse integer.";
+
   @Test
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
 
   @Test
+  public void testString() {
+    Assert.assertEquals(ERROR_STRING, Main.getSumHighestBits(new String[] {"1", "2", "4", "s"}));
+  }
+
+  @Test
+  public void testNegative() {
+    Assert.assertEquals(ERROR_NEGATIVE, Main.getSumHighestBits(new String[] {"1", "-2", "4", "3"}));
+  }
+
+  @Test
   public void testSumHighestBits() {
-    Assert.assertEquals(9, Main.sumHighestBits(new int[] {1, 2, 4, 3}));
-    Assert.assertEquals(0, Main.sumHighestBits(new int[] {0, 0}));
+    Assert.assertEquals("9", Main.getSumHighestBits(new String[] {"1", "2", "4", "3"}));
+    Assert.assertEquals("0", Main.getSumHighestBits(new String[] {"0", "0"}));
+  }
+
+  @Test
+  public void testSumEmpty() {
+    Assert.assertEquals("0", Main.getSumHighestBits(new String[] {}));
   }
 }
