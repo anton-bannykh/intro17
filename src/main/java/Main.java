44c42d451c53e170f1d2b6b94b378033c2a1ac16
PoDuReM <ipoduremennykh@mail.ru>
2017-12-10 04:04:02 +0300

    Task 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-  int foo() {
-    return 10;
+
+  static int sumLowestBits(int[] arr) {
+    int ans = 0;
+    for (int i = 0; i < arr.length; i++) {
+      ans += Integer.lowestOneBit(arr[i]);
+    }
+    return ans;
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,24 @@
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class MainTest {
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testSum() {
+    assertEquals(4, Main.sumLowestBits(new int[] {1, 2, 3}));
+    assertEquals(7, Main.sumLowestBits(new int[] {10, 12, 3}));
+    assertEquals(10, Main.sumLowestBits(new int[] {101, 1, 0, 12, 4}));
+  }
+
+  @Test
+  public void testSumEmpty() {
+    assertEquals(0, Main.sumLowestBits(new int[] {}));
+  }
+
+  @Test
+  public void testSumNegative() {
+    assertEquals(4, Main.sumLowestBits(new int[] {-1, 2, -3}));
+    assertEquals(7, Main.sumLowestBits(new int[] {10, -12, 3}));
+    assertEquals(10, Main.sumLowestBits(new int[] {-101, 1, 0, -12, 4}));
   }
 }
