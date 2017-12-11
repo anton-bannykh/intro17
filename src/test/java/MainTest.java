e5e0f4d019fa0fb0c4df6793591e6b845e89b80a
vorgovo <32040628+vorgovo@users.noreply.github.com>
2017-12-11 11:21:26 +0300

    Update MainTest.java
    added tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,7 +3,20 @@ import org.junit.Test;
 
 public class MainTest {
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void test1_empty() {
+    int[] arr = {};
+    Assert.assertEquals(0, (new Main()).rangeOr(arr));
+  }
+  public void test2_oneNumber() {
+    int[] arr = {1};
+    Assert.assertEquals(1, (new Main()).rangeOr(arr));
+  }
+  public void test3_allBitIsOne() {
+    int[] arr = {-2, 1};
+    Assert.assertEquals(-1, (new Main()).rangeOr(arr));
+  }
+  public void test4_or() {
+    int [] arr = {1, 3};
+    Assert.assertEquals(3, (new Main()).rangeOr(arr));
   }
 }
