6851949bf1f1cb26b905fbb4333f2a6f935ffaa2
unknown <vova@s.w>
2017-12-11 12:09:48 +0300

    tried to do smth like task 3

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,37 +5,49 @@ import org.junit.Test;
 public class MainTest {
   @Test
   public void testNull() {
-    int[] arr = {};
-    assertEquals(0, (new Main()).ssum(arr));
+    String[] arr = {};
+    assertEquals(0, (new Main()).parse(arr));
   }
 
   @Test
   public void testPos() {
-    int[] arr = {6, 6};
-    assertEquals(42, (new Main()).ssum(arr));
+    String[] arr = {"6", "6"};
+    assertEquals(42, (new Main()).parse(arr));
   }
 
   @Test
   public void testNeg() {
-    int[] arr = {-8, -8};
-    assertEquals(56, (new Main()).ssum(arr));
+    String[] arr = {"-8", "-8"};
+    assertEquals(56, (new Main()).parse(arr));
   }
 
   @Test
   public void testZero() {
-    int[] arr = {0, 0};
-    assertEquals(0, (new Main()).ssum(arr));
+    String[] arr = {"0", "0"};
+    assertEquals(0, (new Main()).parse(arr));
   }
 
   @Test
   public void testShort() {
-    int[] arr = {6, -3, 10};
-    assertEquals(1015, (new Main()).ssum(arr));
+    String[] arr = {"6", "-3", "10"};
+    assertEquals(1015, (new Main()).parse(arr));
   }
 
   @Test
   public void testLong() {
-    int[] arr = {-3, -2, -8, 3, 5, 0, 1, -1};
-    assertEquals(2697, (new Main()).ssum(arr));
+    String[] arr = {"-3", "-2", "-8", "3", "5", "0", "1", "-1"};
+    assertEquals(2697, (new Main()).parse(arr));
+  }
+  
+  @Test
+  public void testDouble() {
+    String[] arr = {"-3", "0", "-8", "4.5", "228"};
+    assertEquals("Format isn't correct: \"4.5\" must be integer", (new Main()).parse(arr));
+  }
+  
+  @Test
+  public void testText() {
+    String[] arr = {"1330", "hardGit"};
+    assertEquals("Format isn't correct: \"hardGit\" must be integer", (new Main()).parse(arr));
   }
 }
