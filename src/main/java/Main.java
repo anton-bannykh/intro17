b746b009a00a3133ef4cea5bbb7b75dfa6fa6915
unknown <vova@s.w>
2017-12-11 12:09:48 +0300

    tried to do smth like task 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-  int ssum(int[] arr) { // sum of numbers in index pow
+
+  public static void ssum(String[] arr) { // sum of numbers in index pow
+    System.out.println(new Main().calc(new Main().parse(arr)));
+  }
+
+  int[] parse(String[] arr) {
+    int[] numArr = new int [arr.length];
+    for (int i = 0; i <= arr.length; i++) {
+      try {
+        numArr[i] = Integer.parseInt(arr[i]);
+      } catch (NumberFormatException nfe) {
+        System.out.println("Format isn't correct: \"" + arr[i] + "\" must be integer");
+        System.exit(0);
+      }
+    }
+    return numArr;
+  }
+  
+  int calc(int[] arr) {
     int ans = 0;
     for (int i = 1; i <= arr.length; i++) {
       double pp = Math.pow((double)(arr[i - 1]),(double)i);
