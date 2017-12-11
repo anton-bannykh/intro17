36b3cfe5a00bda663ed826bfcd63f0027190eb71
vorgovo <32040628+vorgovo@users.noreply.github.com>
2017-12-11 10:16:31 +0300

    Update Main.java
    added function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-  int foo() {
-    return 10;
+  public static int rangeOr(int[] arg) {
+    ans = 0;
+    for (int i = 0; i < arg.length; i++) {
+       ans = (ans | arg[i]);
+    }
+    return ans;
   }
 }
