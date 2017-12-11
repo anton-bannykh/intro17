35751ac97483a41abf790ca48cb48e51398fc62b
botal9 <vitaliy.ovechkin.99@mail.ru>
2017-12-12 00:51:41 +0300

    Task2. Added a method calculates sum of highest bits of integer array.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,16 @@ public class Main {
   int foo() {
     return 10;
   }
+
+  static int sumHighestBits(int[] arr) {
+    int ans = 0;
+    for (int i = 0; i < arr.length; i++) {
+      ans += Integer.highestOneBit(arr[i]);
+    }
+    return ans;
+  }
+  
+  /**
+    * Calculates sum of highest bits of given array. 
+  **/
 }
