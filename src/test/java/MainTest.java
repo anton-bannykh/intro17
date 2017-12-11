64c6e86275e9de05d4ae408c3832f4b2ec228942
botal9 <vitaliy.ovechkin.99@mail.ru>
2017-12-12 00:51:41 +0300

    Task2. Added a method calculates sum of highest bits of integer array.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,4 +6,10 @@ public class MainTest {
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
+
+  @Test
+  public void testSumHighestBits() {
+    Assert.assertEquals(9, Main.sumHighestBits(new int[] {1, 2, 4, 3}));
+    Assert.assertEquals(0, Main.sumHighestBits(new int[] {0, 0}));
+  }
 }
