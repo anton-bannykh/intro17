deab4477d2ab96bea9bb2cb2ad223fe0c3e2a421
andrew-chentsov <andryyr@gmail.com>
2017-12-13 19:59:30 +0300

    Task 2 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-  int foo() {
-    return 10;
+
+  int xor(int[] arr) {
+    int ans = 0;
+    for (int i = 0; i < arr.length; i++) {
+      ans ^= arr[i];
+    }
+    return ans;
   }
 }
