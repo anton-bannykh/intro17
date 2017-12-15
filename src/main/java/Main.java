ec9b5466625dd8aac68f853fc1b02b17ddbe0937
Starcall <trubetskoy.b.s@gmail.com>
2017-12-15 22:37:06 +0300

    Сделал второе задание. Тесты проходят, кодстайл тоже

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-  int foo() {
+  int foo() { 
     return 10;
   }
+
+  int getMin(int[] array) {
+    if (array.length == 0) {
+      return Integer.MAX_VALUE;
+    } else {
+      int value = Integer.MAX_VALUE;
+      for (int i = 0; i < array.length; i++) {
+        if (array[i] < value) {
+          value = array[i];
+        }
+      }
+      return value;
+    }
+  }
+
 }
