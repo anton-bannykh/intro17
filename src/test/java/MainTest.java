7985bfa4329c9fd0ce49fe5651d0fd9db6e31ff4
Starcall <trubetskoy.b.s@gmail.com>
2017-12-15 22:10:28 +0300

    Сделал второе задание. Алгоритм прошел тесты и codestyle.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,4 +6,16 @@ public class MainTest {
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
+  
+  @Test
+  public void testRandom() {
+    int[] randomArray = {1, 4, -2, 15};
+    Assert.assertEquals(-2, (new Main()).getMin(randomArray));
+  }
+
+  @Test
+  public void testNull() {
+    int[] nullArray = new int[0];
+    Assert.assertEquals(Integer.MAX_VALUE, (new Main()).getMin(nullArray));
+  }
 }
