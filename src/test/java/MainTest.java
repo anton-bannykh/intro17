490383e643c3f89022f8521585c57157e2abe54d
fr0streaper <andrei1999kashukov@gmail.com>
2017-12-18 01:29:08 +0300

    Add console input and exception test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,21 +5,27 @@ import org.junit.Test;
 public class MainTest {
   @Test
   public void testForZeroes() {
-    assertEquals(5, (new Main()).countTotalZeroes(new int[] {0, 0, 0, 0, 0}));
+    assertEquals("5", (new Main()).countTotalZeroes(new String[] {"0", "0", "0", "0", "0"}));
   }
 
   @Test
   public void testForEmptyArray() {
-    assertEquals(0, (new Main()).countTotalZeroes(new int[] {}));
+    assertEquals("0", (new Main()).countTotalZeroes(new String[] {}));
   }
 
   @Test
   public void testForNoZeroes() {
-    assertEquals(0, (new Main()).countTotalZeroes(new int[] {1, 3, 7, 15, 31}));
+    assertEquals("0", (new Main()).countTotalZeroes(new String[] {"1", "3", "7", "15", "31"}));
   }
 
   @Test
   public void randomTest() {
-    assertEquals(5, (new Main()).countTotalZeroes(new int[] {1, 5, 4, 14, 2}));
+    assertEquals("5", (new Main()).countTotalZeroes(new String[] {"1", "5", "4", "14", "2"}));
+  }
+
+  @Test
+  public void exceptionTest() {
+    assertEquals("Error: \"darude-sandstorm\" is not a number", (new Main())
+        .countTotalZeroes(new String[] {"1", "2", "darude-sandstorm", "5"}));
   }
 }
