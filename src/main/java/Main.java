f4a3ad680a0a573cc515db12ac2f4efc9d838c69
Deymosl <32045215+Deymosl@users.noreply.github.com>
2017-12-18 06:14:49 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-  int foo() {
-    return 10;
+  double chainFrac(int[] arr) {
+    if (arr.length == 0) {
+      System.out.println("Dude, you need numbers");
+      System.exit(0);
+    }
+    double denom = arr[arr.length - 1];
+    int cur = arr.length - 2;
+    while (cur >= 0) {
+      if (denom == 0) {
+        System.out.print("We have a zero in denominator, wrong sequence, it happened at ");
+        System.out.println(cur + 1 + " position, or at least nearby");
+        System.exit(0);
+      }
+      denom = arr[cur] + 1 / denom;
+      cur--;
+    }
+    return denom;
   }
 }
