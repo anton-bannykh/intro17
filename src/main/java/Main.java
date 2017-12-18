871b10920e1bf23ee3f5ad743a5bc6399b08401a
Deymosl <32045215+Deymosl@users.noreply.github.com>
2017-12-18 07:01:15 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,38 @@
 public class Main {
-  int foo() {
-    return 10;
+  public static void main(String[] args) {
+    System.out.println(chainFrac(transStringToInt(args)));
   }
+
+  static int[] transStringToInt(String[] arr) {
+    int[] ans = new int[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      try {
+        ans[i] = Integer.parseInt(arr[i]);
+      } catch (NumberFormatException err) {
+        System.out.println("dude, you sucked at input at " + (i + 1) + " position");
+        System.exit(0);
+      }
+    }
+    return ans;
+  }
+
+  static double chainFrac(int[] arr) {
+    if (arr.length == 0) {
+      System.out.println("Dude, you need numbers");
+      System.exit(0);
+    }
+    double denom = arr[arr.length - 1];
+    int cur = arr.length - 2;
+    while (cur >= 0) {
+      if (denom == 0) {
+        System.out.print("We have a zero in denominator, wrong sequence, it happened at ");
+        System.out.println(cur + 1 + " position, or at least nearby");
+        System.exit(0);
+      }
+      denom = arr[cur] + 1 / denom;
+      cur--;
+    }
+    return denom;
+  }
+
 }
