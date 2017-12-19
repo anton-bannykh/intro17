23a157dc0f690a0ab5dda218d5753c62da9b66ab
Krotkov <krotkovd99@gmail.com>
2017-12-19 20:02:04 +0300

    Обновлены файлы Main.java и MainTest.java. Добавлен метод, считающий сумму количеств единичных битов в массиве, а также несколько тестов к нему.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,14 @@ public class Main {
   int foo() {
     return 10;
   }
+  static int sumOfBits(int[] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += Integer.bitCount(arr[i]);
+    }
+    return sum;
+  }
+  /**
+    *Calculates sum of numbers of 1 bits.
+  **/
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,4 +6,10 @@ public class MainTest {
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
+  @Test
+  public void testSumOfBits() {
+    Assert.assertEquals(0, Main.sumOfBits(new int[] {0, 0}));
+    Assert.assertEquals(5, Main.sumOfBits(new int[] {1, 2, 3, 4}));
+    Assert.assertEquals(3, Main.sumOfBits(new int[] {1, 1, 2}));
+  }
 }
