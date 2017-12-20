05552d0f66a2575b3c7bbf3b3fa759ac2a4fda9a
Starcall <trubetskoy.b.s@gmail.com>
2017-12-20 20:11:14 +0300

    Сделал третье задание, добавил тест на проверку некорректного ввода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     return 10;
   }
 
-  int getMin(int[] array) {
+  static int getMin(int[] array) {
     if (array.length == 0) {
       return Integer.MAX_VALUE;
     } else {
@@ -15,6 +15,22 @@ public class Main {
       }
       return value;
     }
+  }
+
+  static String printMinOrError(String[] args) {
+    int[] curArray = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      try {
+        curArray[i] = Integer.parseInt(args[i]);
+      } catch (NumberFormatException exc) {
+        return new String(args[i] + " is not a number");
+      }
+    }
+    return Integer.toString(getMin(curArray));
+  }
+
+  public static void main(String[] args) {
+    printMinOrError(args);    
   }
 
 }
