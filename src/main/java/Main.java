2563b455bdca00b1ab54ca81ffaeb657c8743524
Артём Борай <borat@MBP-Artem.Dlink>
2017-12-24 20:50:57 +0300

    task 2 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-  int foo() {
+  int foo() { 
     return 10;
   }
+
+  double partSum(double[] ar) {
+    double sum = 0;
+    for (int i = 0; i < ar.length; i++) {
+      sum += 1.0 / ar[i]; 
+    }
+    return sum;
+  }
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,4 +6,14 @@ public class MainTest {
   public void testFoo() {
     Assert.assertEquals(10, (new Main()).foo());
   }
+
+  @Test
+  public void testOk() {
+    double[] array = new double[3];
+    array[0] = 1.0;
+    array[1] = 1.0;
+    array[2] = 1.0;
+    System.out.println((new Main()).partSum(array));
+    Assert.assertEquals(3.0, (new Main()).partSum(array), 0.0);
+  }
 }
