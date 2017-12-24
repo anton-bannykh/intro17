3d0c162c20144e40e0f3e215ff2c0362c23237a4
Krotkov <krotkovd99@gmail.com>
2017-12-24 22:51:08 +0300

    Добавлены новые тесты, данные теперь можно читать из консоли

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,32 @@ public class Main {
     }
     return sum;
   }
+
+  static int[] makeIntArray(String[]  args) throws NumberFormatException {
+    int[] arr = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      arr[i] = Integer.parseInt(args[i]);
+    }
+    return arr;
+  }
+
+  static String makeSumOfBits(String[] args) {
+    try {
+      int[] arr = makeIntArray(args);
+      int ans = sumOfBits(arr);
+      return Integer.toString(ans);
+    } catch (NumberFormatException exc) {
+      return "Can't parse integer";
+    }
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println(makeSumOfBits(args));
+  }
+
   /**
     *Calculates sum of numbers of 1 bits.
+    *Check for errors.
   **/
 }
