0e49980efaff2029588c1eb46440d14bb9cf70ea
gerind <gerind@yandex.ru>
2017-12-24 17:27:22 +0300

    Created packeges

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,18 @@
-public class Main {
-  /**
-   * @param mas Array of integers.
-   * @param args Array of arguments from command line.
-   */
-  public static boolean get_args(int[] mas, String[] args) {
-    for (int i = 0; i < args.length; i++) {
-      try {
-        mas[i] = Integer.parseInt(args[i]);
-      } catch (Exception exception) {
-        return false;
-      }
-    }
-    return true;
-  }
+import argstoarray.ArgsToArray;
+import checksum.CheckSum;
 
+public class Main {
   /**
    * @param args Array of strings given from console.
    */
   public static void main(String[] args) {
     int[] mas = new int[args.length];
-    if (get_args(mas, args) == false) {
+    if (ArgsToArray.get_args(mas, args) == false) {
       System.out.println("Wrong input data!");
-    } else if (foo(mas)) {
+    } else if (CheckSum.foo(mas)) {
       System.out.println("Yes");
     } else {
       System.out.println("No");
     }
-  }
-
-  /**
-   * @param mas Array of integers.
-   * @return boolean True if sum of integers is even.
-   */
-  public static boolean foo(int[] mas) {
-    int sum = 0;
-    for (int cnt = 0; cnt < mas.length; cnt++) {
-      sum ^= mas[cnt];
-    }
-    return ((sum & 1) == 0);
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,86 +1,88 @@
 import org.junit.Assert;
 import org.junit.Test;
+import argstoarray.ArgsToArray;
+import checksum.CheckSum;
 
 public class MainTest {
   @Test
   public void simpleTest1() {
-    Assert.assertEquals(true, (new Main()).foo(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}));
+    Assert.assertEquals(true, CheckSum.foo(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}));
   }
 
   @Test
   public void simpleTest2() {
-    Assert.assertEquals(false, (new Main()).foo(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    Assert.assertEquals(false, CheckSum.foo(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
   }
 
   @Test
   public void simpleTest3() {
-    Assert.assertEquals(false, (new Main()).foo(new int[]{1}));
+    Assert.assertEquals(false, CheckSum.foo(new int[]{1}));
   }
 
   @Test
   public void simpleTest4() {
-    Assert.assertEquals(true, (new Main()).foo(new int[]{2}));
+    Assert.assertEquals(true, CheckSum.foo(new int[]{2}));
   }
 
   @Test
   public void testOverflow1() {
-    Assert.assertEquals(false, (new Main()).foo(new int[]{2147483647, 2147483646,
+    Assert.assertEquals(false, CheckSum.foo(new int[]{2147483647, 2147483646,
         2147483645, 2147483644, 2147483643, 2147483642}));
   }
 
   @Test
   public void testOverflow2() {
-    Assert.assertEquals(false, (new Main()).foo(new int[]{-2147483648, -2147483647,
+    Assert.assertEquals(false, CheckSum.foo(new int[]{-2147483648, -2147483647,
         -2147483646, -2147483645, -2147483644, -2147483643}));
   }
 
   @Test
   public void testOverflow3() {
-    Assert.assertEquals(true, (new Main()).foo(new int[]{-2147483648, 2147483647, -2147483647,
+    Assert.assertEquals(true, CheckSum.foo(new int[]{-2147483648, 2147483647, -2147483647,
         2147483646, -2147483646, 2147483645, -2147483645, 2147483644, -2147483644, 2147483643,
         -2147483643, 2147483642}));
   }
 
   @Test
   public void testCorrectInput1() {
-    Assert.assertEquals(false, (new Main()).get_args(new int[3], new String[]{"1", "4", "ups"}));
+    Assert.assertEquals(false, ArgsToArray.get_args(new int[3], new String[]{"1", "4", "ups"}));
   }
 
   @Test
   public void testCorrectInput2() {
-    Assert.assertEquals(true, (new Main()).get_args(new int[3], new String[]{"1", "4", "3"}));
+    Assert.assertEquals(true, ArgsToArray.get_args(new int[3], new String[]{"1", "4", "3"}));
   }
 
   @Test
   public void testCorrectInput3() {
-    Assert.assertEquals(true, (new Main()).get_args(new int[12], new String[]{"-2147483648",
+    Assert.assertEquals(true, ArgsToArray.get_args(new int[12], new String[]{"-2147483648",
         "2147483647", "-2147483647", "2147483646", "-2147483646", "2147483645", "-2147483645",
         "2147483644", "-2147483644", "2147483643", "-2147483643", "2147483642"}));
   }
 
   @Test
   public void testCorrectInput4() {
-    Assert.assertEquals(false, (new Main()).get_args(new int[12], new String[]{"-2147483648",
+    Assert.assertEquals(false, ArgsToArray.get_args(new int[12], new String[]{"-2147483648",
         "2147483647", "-2147483647", "2147483646", "-2147483646", "2147483645", "-2I47483645",
         "2147483644", "-2147483644", "2147483643", "-2147483643", "2147483642"}));
   }
 
   @Test
   public void testCorrectInput5() {
-    Assert.assertEquals(false, (new Main()).get_args(new int[12], new String[]{"-2147483648",
+    Assert.assertEquals(false, ArgsToArray.get_args(new int[12], new String[]{"-2147483648",
         "2147483647", "--2147483647", "2147483646", "-2147483646", "2147483645", "-2147483645",
         "2147483644", "-2147483644", "2147483643", "-2147483643", "2147483642"}));
   }
-  
+
   @Test
   public void testCorrectInput6() {
-    Assert.assertEquals(true, (new Main()).get_args(new int[12], new String[]{"-2147483648",
+    Assert.assertEquals(true, ArgsToArray.get_args(new int[12], new String[]{"-2147483648",
         "2147483647", "+2147483647", "2147483646", "-2147483646", "2147483645", "-2147483645",
         "2147483644", "-2147483644", "2147483643", "-2147483643", "2147483642"}));
   }
 
   @Test
   public void testCorrectInput7() {
-    Assert.assertEquals(true, (new Main()).get_args(new int[0], new String[0]));
+    Assert.assertEquals(true, ArgsToArray.get_args(new int[0], new String[0]));
   }
 }
