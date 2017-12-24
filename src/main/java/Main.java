fb68697bdf6b96cf44a437f46787d32b79ea8340
vorgovo <32040628+vorgovo@users.noreply.github.com>
2017-12-25 01:52:16 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,23 @@
 public class Main {
-  public static int rangeOr(int[] arg) {
-    int ans;
-    ans = 0;
-    for (int i = 0; i < arg.length; i++) {
-       ans = (ans | arg[i]);
+    public static void main(String[] args) {
+        System.out.println(consoleOr(args));
     }
-    return ans;
-  }
+    public static String consoleOr(String[] args) {
+        int[] input = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            try {
+                input[i] = Integer.parseInt(args[i]);
+            } catch (NumberFormatException error) {
+                return String.format("Infernal error: string %d not a number", i);
+            }
+        }
+        return Integer.toString(rangeOr(input));
+    }
+    public static int rangeOr(int[] arg) {
+        int ans = 0;
+        for (int i = 0; i < arg.length; i++) {
+            ans = (ans | arg[i]);
+        }
+        return ans;
+    }
 }
