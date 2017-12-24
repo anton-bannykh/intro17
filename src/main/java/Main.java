2a24c90afe874585edac4f227be045458da835e7
Артём Борай <borat@MBP-Artem.Dlink>
2017-12-24 23:39:31 +0300

    task 3 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,28 @@ public class Main {
     return 10;
   }
 
-  double partSum(double[] ar) {
+  static double partSum(double[] ar) {
     double sum = 0;
     for (int i = 0; i < ar.length; i++) {
       sum += 1.0 / ar[i]; 
     }
     return sum;
+  }
+
+  static String checkError(String[] ar) {
+    double[] curAr = new double[ar.length];
+    for (int i = 0; i < ar.length; i++) {
+      try {
+        curAr[i] = Double.parseDouble(ar[i]);
+      } catch (NumberFormatException exc) {
+        return new String(ar[i] + " is not a number");
+      }
+    }
+    return Double.toString(partSum(curAr));
+  }
+
+  public static void main(String[] ar) {
+    checkError(ar);    
   }
 
 }
