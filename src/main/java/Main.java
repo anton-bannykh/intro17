d2bbdc7b858c69fa049b622960823785910c4022
Xpech <lisgml@gmail.com>
2017-12-25 01:39:59 +0300

    Implemented task 2 and tests for method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
+import java.math.BigInteger;
+
 public class Main {
-  int foo() {
-    return 10;
+  int countZeroes(int[] arr) {
+
+    if (arr.length == 0) {
+      return 0;
+    }
+
+    BigInteger result = BigInteger.ONE;
+    BigInteger buffer;
+    for (int i : arr) {
+      buffer = BigInteger.valueOf(i);
+      result = result.multiply(buffer);
+    }
+
+    int answer = 0;
+
+    BigInteger ten = BigInteger.valueOf(10);
+    for (;!result.equals(BigInteger.ZERO);result = result.divide(ten)) {
+      if (result.mod(ten).equals(BigInteger.ZERO)) {
+        answer++;
+      }
+    }
+
+    return answer;
+
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,7 +3,19 @@ import org.junit.Test;
 
 public class MainTest {
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testCountSimple() {
+    int[] testArr = {10, 20, 30};
+    Assert.assertEquals(3, (new Main()).countZeroes(testArr));
+  }
+
+  @Test
+  public void testEmptyInput() {
+    Assert.assertEquals(0, (new Main()).countZeroes(new int[]{}));
+  }
+
+  @Test
+  public void testBigInt() {
+    int[] testArr = {1000000000, 1000000000, 1000000000, 1000000000, 10000};
+    Assert.assertEquals(40, (new Main()).countZeroes(testArr));
   }
 }
