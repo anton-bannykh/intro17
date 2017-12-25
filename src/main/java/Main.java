fca4c5406a99df3b76fdb76251ccb2658ab5acb1
Yaroslav <zhurba_ys@mail.ru>
2017-12-25 10:36:48 +0300

    Codestyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,19 @@
 public class Main {
-  int fun (int arr[])
-  {
+
+  int fun(int[] arr) {
     if (arr.length == 0) {
       System.out.println(0);
       System.exit(0);
     }
     int sum = 0;
-
     for (int i = 0; i < arr.length; i++) {
-      String a;
-      a = arr[i] + "";
-      for (int j = 0; j < a.length(); j++) {
-          if (a.charAt(j) == '0')
-              sum++;
+      String str = arr[i] + "";
+      for (int j = 0; j < str.length(); j++) {
+        if (str.charAt(j) == '0') {
+          sum++;
+        }
       }
-
-
     }
-
-
     return sum;
   }
-
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,22 +6,27 @@ public class MainTest {
   public void testFun1() {
     Assert.assertEquals(0, (new Main()).fun(new int[] {1, 2}));
   }
+
   @Test
   public void testFun2() {
     Assert.assertEquals(3, (new Main()).fun(new int[] {10, 34, 3080}));
   }
+
   @Test
   public void testFun3() {
     Assert.assertEquals(6, (new Main()).fun(new int[] {10, 100, 1000}));
   }
+
   @Test
   public void testFun4() {
     Assert.assertEquals(3, (new Main()).fun(new int[] {1010, 239, 228, 0}));
   }
+
   @Test
   public void testFun5() {
     Assert.assertEquals(10, (new Main()).fun(new int[] {10, 10, 10, 10, 10, 10, 10, 10, 10, 10}));
   }
+
   @Test
   public void testFun6() {
     Assert.assertEquals(1, (new Main()).fun(new int[] {1, 2,3,4,5,6,30,239,228}));
