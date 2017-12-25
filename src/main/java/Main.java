2214359a0dd862e29f2e8dfc6ec96ab9e87b849d
Yaroslav <zhurba_ys@mail.ru>
2017-12-25 09:36:58 +0300

    Add fun and test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-  int foo() {
-    return 10;
+  int fun (int arr[])
+  {
+    if (arr.length == 0) {
+      System.out.println(0);
+      System.exit(0);
+    }
+    int sum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      String a;
+      a = arr[i] + "";
+      for (int j = 0; j < a.length(); j++) {
+          if (a.charAt(j) == '0')
+              sum++;
+      }
+
+
+    }
+
+
+    return sum;
   }
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,7 +3,29 @@ import org.junit.Test;
 
 public class MainTest {
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testFun1() {
+    Assert.assertEquals(0, (new Main()).fun(new int[] {1, 2}));
+  }
+  @Test
+  public void testFun2() {
+    Assert.assertEquals(3, (new Main()).fun(new int[] {10, 34, 3080}));
+  }
+  @Test
+  public void testFun3() {
+    Assert.assertEquals(6, (new Main()).fun(new int[] {10, 100, 1000}));
+  }
+  @Test
+  public void testFun4() {
+    Assert.assertEquals(3, (new Main()).fun(new int[] {1010, 239, 228, 0}));
+  }
+  @Test
+  public void testFun5() {
+    Assert.assertEquals(10, (new Main()).fun(new int[] {10, 10, 10, 10, 10, 10, 10, 10, 10, 10}));
+  }
+  @Test
+  public void testFun6() {
+    Assert.assertEquals(1, (new Main()).fun(new int[] {1, 2,3,4,5,6,30,239,228}));
   }
 }
+
+
