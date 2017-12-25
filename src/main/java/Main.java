d22e9fb10957787722964ca06bac08a305fb7ef7
zelyanin <32039715+zelyanin@users.noreply.github.com>
2017-12-26 00:16:12 +0300

    Task 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 public class Main {
-  int foo(int[] ms) {
-    int sum=0;
-    for (int i = 0; i < ms.length; i++)
-	{
-		sum += ms[i];
-	}
-	return sum;
-  }
+    public static void Main(String[] args) {
+    System.out.println(foo(args));
+    }
+    static String foo(String[] ms) {
+        try {
+            int sum = 0;
+            for (int i = 0; i < ms.length; i++) {
+                sum += Integer.parseInt(ms[i]);
+            }
+            return Integer.toString(sum);
+        }
+        catch (NumberFormatException ex) {
+            return "Input isn't correct...";
+        }
+    }
 }
