6fd7ff6c63130c956c69f7bf89cdc869fc9be97c
Typis <34036984+Typis@users.noreply.github.com>
2017-12-26 08:36:23 +0300

    Task 2-3

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,8 +2,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
+
   @Test
-  public void testFoo() {
-    Assert.assertEquals(10, (new Main()).foo());
+  public void testSumString() {
+    Assert.assertEquals("4", Main.getArgs(new int[] {"2", "2"}));
+    Assert.assertEquals("15", Main.getArgs(new String[] {"1", "2", "2", "10"}));
   }
+
+  @Test
+  public void testStringError() {
+    Assert.assertEquals("string is not a number", 
+        Main.getArgs(new String[] {"1","7899", "string", "567", "222"}));
+    Assert.assertEquals("+ is not a number", 
+        Main.getArgs(new String[] {"1", "2", "+", "10"}));
+  }
+
+  @Test
+  public void testSumEmpty() {
+    Assert.assertEquals("Imput is empty", Main.getArgs(new String[] {}));
+  }
+
 }
